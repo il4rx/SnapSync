f6faa5bf1d60022a746aff55c7dfaecbb12b0fc3
il4rx <dockname12@gmail.com>
2023-12-28 02:07:57 +0700

    Updated Format And Syntax

--- enterance.py
+++ enterance.py
@@ -1,10 +1,10 @@
 import os
-import time, datetime
+import time
+import datetime
 import mysql.connector
 import pyfiglet
-from pyfiglet import figlet_format
 import colorama
-from colorama import Fore, Back, Style
+from colorama import *
 import bcrypt
 import random
 import subprocess
@@ -12,77 +12,82 @@ import sys
 
 date = datetime.datetime.now()
 _strf = date.strftime("%x")
-
-
 _connector = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="",
-  database="snap_snyc"
+    host="localhost", user="root", password="", database="snap_snyc"
 )
-
 _cursor = _connector.cursor()
 
+class Figlet:
+    @staticmethod
+    def header():
+        return pyfiglet.figlet_format("SnapSync")
 
-class Figlet :
-  def header() :
-    figlet = figlet_format("SnapSync")
-    return figlet
-  def description() :
-    description = f"""
+    @staticmethod
+    def description():
+        return f"""
 Welcome to SnapSync, You Can Create Your Account Here,
-Use `{Fore.GREEN}--create{Style.RESET_ALL}, after it
-use `{Fore.GREEN}--start{Style.RESET_ALL}` then to login use, `{Fore.GREEN}--login{Style.RESET_ALL}
-Enjoy your forums,
+Use {Fore.GREEN}--create{Style.RESET_ALL} to create your account or make a new account, after it
+use {Fore.GREEN}--start{Style.RESET_ALL}, to start the forum (you need to log in again)
 
 Acknowledgement :
-To Start Post use `{Fore.GREEN}-e{Style.RESET_ALL}` or `{Fore.GREEN}--post{Style.RESET_ALL}`, to delete your post `{Fore.GREEN}-pd {Fore.YELLOW}[messageID]{Style.RESET_ALL}` or
-`{Fore.GREEN}--post_delete {Fore.YELLOW}[messageID]{Style.RESET_ALL}` or `{Fore.GREEN}--delete {Fore.YELLOW}[messageID]{Style.RESET_ALL}`. only admin has access
+To Start Post use {Fore.GREEN}-e{Style.RESET_ALL} or {Fore.GREEN}--post{Style.RESET_ALL}, to delete your post {Fore.GREEN}-pd {Fore.YELLOW}[messageID]{Style.RESET_ALL} or
+{Fore.GREEN}--post_delete {Fore.YELLOW}[messageID]{Style.RESET_ALL} or {Fore.GREEN}--delete {Fore.YELLOW}[messageID]{Style.RESET_ALL}. only admin has access
 to your message, and can delete it. or maybe deactivating your account.
-    """
-    return description
+        """
 
+class Main:
+    @staticmethod
+    def create_account():
+        username = input("Username : >>> ")
+        password = input("Password : >>> ")
+        password = password.encode("utf-8")
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt(10))
+        user_id = f"{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}"
+        try:
+            print("Creating Account...")
+            time.sleep(3)
+            query = "INSERT INTO `users`(`user_id`, `username`, `password`, `date`) VALUES (%s, %s, %s,%s)"
+            params = (user_id, username, hashed_password, _strf)
+            _cursor.execute(query, params)
+            _connector.commit()
+        finally:
+            print("Your Account Successfully Created!")
+            time.sleep(2)
 
-class Main :
-  def Body() :
-    print(f"{Fore.CYAN}[+]{Style.RESET_ALL} Please Input Any Commands")
-    userInput = input(">>> ")
+    @staticmethod
+    def start_program():
+        try:
+            print("Starting Program....")
+            time.sleep(1)
+            subprocess.Popen(["python", "main.py"])
+            os.startfile("main.py")
+            sys.exit(0)
+        except KeyboardInterrupt:
+            print("Error!")
 
-    if userInput == "--create":
-      username_input = input("Username : >>> ")
-      password_input = input("Password : >>> ")
-      
-      password_input = password_input.encode('utf-8')
-      password_hash = bcrypt.hashpw(password_input, bcrypt.gensalt(10))
-      user_ids = int(f"{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}{random.randint(0,9)}")
-      
-      try :
-        print("Creating Account...")
-        time.sleep(5)
-        
-        prompt = "INSERT INTO `users`(`user_id`, `username`, `password`, `date`) VALUES (%s, %s, %s,%s)"
-        values = (user_ids, username_input, password_hash, _strf)
-        _cursor.execute(prompt, values)
-        _connector.commit()
-        
-      finally :
-        print("Your Account Successfully Created!")
-        time.sleep(2)
-    pass
+    @staticmethod
+    def body():
+        while True:
+            print(Figlet.header())
+            time.sleep(1)
+            print(Figlet.description())
+            time.sleep(1)
+            print(f"{Fore.CYAN}[+]{Style.RESET_ALL} Please Input Any Commands")
+            command = input(">>> ")
+            if command == "--create":
+                Main.create_account()
+                os.system("cls||clear")
+            elif command == "--start":
+                os.system("cls||clear")
+                Main.start_program()
+            else:
+                print("Invalid command")
+
+def main():
+    Figlet.header()
+    Figlet.description()
+    Main.body()
   
-    if userInput == "--start":
-      print("ok")
-    
-    
-    
-def Lexer():
-  while True:
-    print(Figlet.header())
-    time.sleep(1)
-    print(Figlet.description())
-    time.sleep(1)
-    
-    Main.Body()
-    os.system("cls||clear")
-  
-Lexer()
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,11 @@
+import os
+import time, datetime
+import mysql.connector
+import pyfiglet
+from pyfiglet import figlet_format
+import colorama
+from colorama import Fore, Back, Style
+import bcrypt
+import random
+import subprocess
+import sys
