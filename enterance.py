fd83347531dc9df1121dd453ffb6ce1a40525fd3
il4rx <dockname12@gmail.com>
2023-12-27 23:32:21 +0700

    Update

--- enterance.py
+++ enterance.py
@@ -2,7 +2,9 @@ import os
 import time, datetime
 import mysql.connector
 import pyfiglet
-
+from pyfiglet import figlet_format
+import colorama
+from colorama import Fore, Back, Style
 
 _connector = mysql.connector.connect(
   host="localhost",
@@ -11,3 +13,32 @@ _connector = mysql.connector.connect(
   database="snap_snyc"
 )
 
+_cursor = _connector.cursor()
+
+
+class Figlet :
+  def header() :
+    figlet = figlet_format("SnapSync")
+    return figlet
+  def description() :
+    description = f"""
+Welcome to SnapSync, You Can Create Your Account Here,
+Use `{Fore.GREEN}--create {Fore.YELLOW}[Account username] {Fore.RED}[password]{Style.RESET_ALL}, after it
+use `{Fore.GREEN}--start{Style.RESET_ALL}` then to login use, `--login [username] [password]`
+Enjoy your forums,
+
+Acknowledgement :
+To Start Post use `-e` or `--post`, to delete your post `-pd [messageID]` or
+`--post_delete [messageID]` or `--delete [messageID]`. only admin has access
+to your message, and can delete it. or maybe deactivating your account.
+    """
+    return description
+  
+  
+def Lexer():
+  print(Figlet.header())
+  time.sleep(1)
+  print(Figlet.description())
+  
+  
+Lexer()
